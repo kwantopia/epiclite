18b7760956d3e27d84c5c5c714801091e22d5d02
Kwan Hong Lee <kwan@redstar.com>
2014-02-05 00:49:41 -0500

    still fixing static issue

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn epiclite.wsgi
+web: python manage.py collectstatic --noinput; gunicorn epiclite.wsgi
--- epiclite/settings.py
+++ epiclite/settings.py
@@ -102,12 +102,11 @@ USE_TZ = True
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-PROJECT_PATH = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_PATH, 'static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 MEDIA_ROOT = 'media'
